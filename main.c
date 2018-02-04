e37d2bb55ad1ffe8ec1228ef1985445e6545114f
beingash22 <ashrockzap@gmail.com>
2018-02-04 20:05:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,10 +5,22 @@ void main()
 int a, b;
 
 scanf ("%d\t%d", &a, &b);
-
+if (a>=0 && b>=0)
+{
 for (int i=a+1; i<b; i++)
 {
 if (i%2!=0)
 printf ("%d\t", i);
+}
+}
+else
+{
+    a=a*-1;
+    b=b*-1;
+    for (int i=a+1; i<b; i++)
+    {
+    if (i%2!=0)
+    printf ("%d\t", i);
+    }
 }
 }
